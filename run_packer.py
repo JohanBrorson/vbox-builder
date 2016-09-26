807e119b66559ba15ac92f2877ef108b67ae6ca1
Johan Brorson <johan_brorson@hotmail.com>
2016-09-26 20:46:15 +0200

    Add possibility to skip updates

--- run_packer.py
+++ run_packer.py
@@ -5,14 +5,16 @@ Script for building a VirtualBox image with Packer
 import argparse
 import os
 import subprocess
+import sys
 
 AUTOUNATTEND_TEMPLATE = './answerfiles/windows10/Autounattend.xml.template'
 AUTOUNATTEND_XML = './answerfiles/windows10/Autounattend.xml'
 
-def run_packer(username, password):
+def run_packer(username, password, skip_window_updates):
     write_autounattend_file(username, password)
     os.environ['WINRM_USERNAME'] = username
     os.environ['WINRM_PASSWORD'] = password
+    os.environ['SKIP_WINDOWS_UPDATES'] = str(skip_window_updates)
     os.environ['PACKER_LOG'] = '1'
     os.environ['PACKER_LOG_PATH'] = 'packer.log'
     subprocess.check_call(['/opt/packer/packer', 'build', '-var-file=variables.json', 'windows_10.json'])
@@ -28,7 +30,7 @@ def write_autounattend_file(username, password):
 def main():
     parser = argparse.ArgumentParser(
         description=('Script for building a VirtualBox image with Packer'))
-    parser.set_defaults(screenshots=True)
+    parser.set_defaults(skip_window_updates=False)
     parser.add_argument('-u', '--username',
                         dest='username',
                         action='store',
@@ -39,10 +41,16 @@ def main():
                         action='store',
                         required=True,
                         help='The users password')
+    parser.add_argument('-n', '--no-updates',
+                        dest='skip_window_updates',
+                        action='store_true',
+                        required=False,
+                        help='Skip installing Windows updates')
 
     args = parser.parse_args()
     run_packer(args.username,
-               args.password)
+               args.password,
+               args.skip_window_updates)
 
 if __name__ == '__main__':
     main()
