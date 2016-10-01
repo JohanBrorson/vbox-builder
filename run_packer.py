172a69077cc5f33ecd3560fd94c66cf9b19905c5
Johan Brorson <johan_brorson@hotmail.com>
2016-10-01 13:17:47 +0200

    Run pylint with Travis CI

--- run_packer.py
+++ run_packer.py
@@ -5,21 +5,22 @@ Script for building a VirtualBox image with Packer
 import argparse
 import os
 import subprocess
-import sys
 
 AUTOUNATTEND_TEMPLATE = './answerfiles/windows10/Autounattend.xml.template'
 AUTOUNATTEND_XML = './answerfiles/windows10/Autounattend.xml'
 
 def run_packer(username, password, skip_window_updates):
+    """Runs the packer command"""
     write_autounattend_file(username, password)
     os.environ['WINRM_USERNAME'] = username
     os.environ['WINRM_PASSWORD'] = password
     os.environ['SKIP_WINDOWS_UPDATES'] = str(skip_window_updates)
     os.environ['PACKER_LOG'] = '1'
     os.environ['PACKER_LOG_PATH'] = 'packer.log'
-    subprocess.check_call(['/opt/packer/packer', 'build', '-var-file=variables.json', 'windows_10.json'])
+    subprocess.check_call(['packer', 'build', '-var-file=variables.json', 'windows_10.json'])
 
 def write_autounattend_file(username, password):
+    """Write the autounattend file with username and password"""
     template = open(AUTOUNATTEND_TEMPLATE).read()
     autounattend_content = template.replace('${username}', username)
     autounattend_content = autounattend_content.replace('${password}', password)
@@ -28,6 +29,7 @@ def write_autounattend_file(username, password):
     autounattend_file.close()
 
 def main():
+    """"Main function"""
     parser = argparse.ArgumentParser(
         description=('Script for building a VirtualBox image with Packer'))
     parser.set_defaults(skip_window_updates=False)
