da208af5c814e1f5a170f3b8263af06e9031125b
Johan Brorson <johan_brorson@hotmail.com>
2018-11-19 14:15:42 +0100

    Update version of Windows and software

--- scripts/Download-JDK.ps1
+++ scripts/Download-JDK.ps1
@@ -1,6 +1,6 @@
-$JdkVersion = "8u171"
-$FileName = "jdk-${JdkVersion}-windows-x64.exe"
-$Source = "http://download.oracle.com/otn-pub/java/jdk/${JdkVersion}-b11/512cd62ec5174c3487ac17c61aaa89e8/${FileName}"
+$JdkVersion = "11.0.1"
+$FileName = "jdk-${JdkVersion}_windows-x64_bin.exe"
+$Source = "http://download.oracle.com/otn-pub/java/jdk/${JdkVersion}+13/90cf5d8f270a4347a95050320eef3fb7/${FileName}"
 $Destination = "$Env:USERPROFILE\Downloads\$FileName"
 $Client = New-Object System.Net.WebClient
 $Cookie = "oraclelicense=accept-securebackup-cookie"
